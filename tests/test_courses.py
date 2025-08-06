99fbcd020edb983317b04f8f52643dccc88302c0
Anychnyi_Anton <anychnyianton@gmail.com>
2025-08-06 06:28:00 +0400

    Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,7 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect, Page
+from playwright.sync_api import expect
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 
 
 @pytest.mark.regression
@@ -20,20 +22,23 @@ def test_empty_courses_list(chromium_page_with_state):
     expect(courses_page_no_results).to_have_text("There is no results")
     print("Заголовок пустого блока 'There is no results' найден")
 
-@pytest.mark.regression
-@pytest.mark.courses
-def test_empty_courses_list_2(chromium_page_with_state):
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
     """
-    Тест пустого списка курсов
+    Тест создания курса
     """
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")  # переход на сайт
-
-    courses_page_title = chromium_page_with_state.locator(
-        '//h6[@data-testid="courses-list-toolbar-title-text"]')  # поиск заголовка страницы
-    expect(courses_page_title).to_have_text("Courses")
-    print("Заголовок страницы 'Courses' найден")
-
-    courses_page_no_results = chromium_page_with_state.locator(
-        '//h6[@data-testid="courses-list-empty-view-title-text"]')  # поиск заголовка страницы
-    expect(courses_page_no_results).to_have_text("There is no results")
-    print("Заголовок пустого блока 'There is no results' найден")
+    create_course_page.open_url("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form("", "", "", "0", "0")
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image("C:\\Users\\anton\PycharmProjects\\autotests-ui\\testdata\\files\\image.jpg")
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.fill_create_course_form("Playwright", "2 weeks", "Playwright", "100", "10")
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(0, "Playwright", "100", "10", "2 weeks")
--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,4 +1,3 @@
-from playwright.sync_api import Page
 import pytest
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
@@ -7,13 +6,11 @@ from pages.dashboard_page import DashboardPage
 @pytest.mark.regression
 @pytest.mark.registration
 @pytest.mark.smoke
-def test_user_registration_positive(start_chrome: Page):
+def test_user_registration_positive(dashboard_page: DashboardPage, registration_page: RegistrationPage):
     """
     Тест успешной регистрации пользователя
     """
-    registration_page = RegistrationPage(start_chrome)
     registration_page.open_url("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
     registration_page.fill_registration_form("s7w8w@example.com", "username", "password")
     registration_page.click_registration_button()
-    dashboard_page = DashboardPage(start_chrome)
     dashboard_page.check_visible_dashboard_title()
